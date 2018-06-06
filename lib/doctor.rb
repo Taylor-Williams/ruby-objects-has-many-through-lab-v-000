fa149b57b486cc76ac4b741249af6108e5b15d32
Taylor Williams <taylor@thewilliams.net>
2018-06-06 23:37:37 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,14 @@
 class Doctor
   attr_accessor :name, :patients
-
+  @@all = []
+  
   def initialize(name)
     @name = name
     @patients = []
+    @@all << self
   end
 
+  def self.all
+    @@all?
+  end
 end
