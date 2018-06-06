22e88724dd22166f24071dc8af7d5ff0439248ab
Taylor Williams <taylor@thewilliams.net>
2018-06-06 23:41:33 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,14 @@
 class Appointment
-  
+
   attr_accessor :date, :doctor, :patient
 
   @@all = []
-  
+
   def initialize(date, patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
+    @@all << self
   end
 
   def self.all
