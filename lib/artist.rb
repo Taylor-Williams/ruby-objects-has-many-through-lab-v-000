fb988c90d4eb55cde1bfeffbec469807e11bca84
Taylor Williams <taylor@thewilliams.net>
2018-06-06 23:08:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,4 +12,10 @@ class Artist
   def self.all
     @@all
   end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    songs << song
+    genres << genre if !genres.includes?(genre)
+  end
 end
